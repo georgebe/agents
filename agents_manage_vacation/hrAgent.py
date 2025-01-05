7db63ef425c37c655c54974db8f888d29e386815
George Belsian <georaj.be@gmail.com>
2025-01-05 17:05:38 -0500

    removed incomplete code in hrAgent file

--- agents_manage_vacation/hrAgent.py
+++ agents_manage_vacation/hrAgent.py
@@ -1,4 +1,4 @@
-from phi.agent import Agent, AgentMemory
+from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from VacationTools import VacationTools
 from dotenv import load_dotenv
@@ -18,9 +18,8 @@ hr_agent = Agent(
     reasoning=True
 )
 
-#hr_agent.print_response("Summarize analyst recommendations for NVDA", stream=True)
 hr_agent.print_response("How much vacation does employee_id 1 have available?", stream=True)
 hr_agent.print_response("great. please reserve one day of time off, June 1 2024", stream=True)
 
-hr_agent.
+
 #hr_agent.print_response("now let me take the last 3 months of the year off as vacation, from Oct 1 2024 through Dec 31 2024", stream=True)
